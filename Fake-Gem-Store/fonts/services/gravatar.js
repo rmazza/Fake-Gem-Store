cd3a3c4d47df30e0443349419881a0756203d401
rmazza <robertmazza1@gmail.com>
2016-10-10 10:39:50 -0500

    removed junk

--- Fake-Gem-Store/fonts/services/gravatar.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp')
-    .factory('Gravatar',function gravatarFactory(){
-        var avatarSize = 80;
-        var avatarUrl = "http://www.gravatar.com/avatar/";
-        return function(email){
-                return avatarUrl + CryptoJS.MD5(email).toString() + "?size" + avatarSize.toString();
-        };
-    });
--- Fake-Gem-Store/fonts/services/product-service.js
+++
@@ -1,8 +0,0 @@
-angular.module("myApp")
-    .factory('productService', function productServiceFactory($http) {
-        return {
-            get: function () {
-                return $http({ method: 'GET', url: '../d/products.json' });
-            }
-        };
-    });
