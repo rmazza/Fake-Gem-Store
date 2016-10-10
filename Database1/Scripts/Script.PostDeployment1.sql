c9393efd60ea251fc4a6a9c3f5a43cebdf550b0d
rmazza <robertmazza1@gmail.com>
2016-10-10 11:14:47 -0500

    added post-deploy sql script

---
+++ Database1/Scripts/Script.PostDeployment1.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO Product(name,[price],soldOut,canPurchase,[description],quantity)
+VALUES
+('A Huge Rock', 13.99, 1, 1,'A very big rock', 12)
